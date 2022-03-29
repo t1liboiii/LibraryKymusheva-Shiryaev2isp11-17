d13d9aae608ef53ae5d43ec6af8ff17577f8d15b
IMac3 <IMac3>
2022-03-29 14:49:25 +0300

    .

--- LibraryShiryaev2isp11-17/ClassHelper/Penalty.cs
+++ LibraryShiryaev2isp11-17/ClassHelper/Penalty.cs
@@ -7,60 +7,21 @@ using LibraryShiryaev2isp11_17.ClassHelper;
 using LibraryShiryaev2isp11_17.EF;
 namespace LibraryShiryaev2isp11_17.ClassHelper
 {
-    class Penalty
+   public static class Penalty
     {
-        public static double DebtClient(int IDClient)
+        public static double DebtClient(DateTime dateStart, double price)
         {
-            var issues = AppData.Context.Issue.ToList().Where(i => i.ClientID == IDClient);
-            var books = AppData.Context.Book.ToList();
-            double result = 0;
-
-            foreach (Issue issue in issues)
+            int dayCount = (DateTime.Now - dateStart).Days;
+            double debt = (dayCount - 30) * 0.01 * price;
+            if (debt >0)
             {
-                DateTime dateReturn = (DateTime)issue.DateReturn;
-                DateTime? dateTurnIn = issue.DateTrunIn;
-                DateTime dateEnd;
-                if (dateTurnIn == null)
-                {
-                    dateEnd = DateTime.Now;
-                }
-                else
-                {
-                    dateEnd = (DateTime)dateTurnIn;
-                }
-                TimeSpan interval = dateEnd - dateReturn;
-                double days = interval.TotalDays;
-                if (days > 30)
-                {
-                    int BookID = (int)issue.BookID;
-                    Book book = books.FirstOrDefault(i => i.BookID == BookID);
-                    double price = (double)book.Price;
-                    result += price / 100 * (days - 30);
-                }
-            }
-                
-            return result;
-        }
-        public static double Debt(double bookPrice, DateTime dateReturn, DateTime? dateTurnIn, bool isPaidFor)
-        {
-            if (isPaidFor)
-            {
-                return 0;
-            }
-            DateTime dateEnd;
-            if (dateTurnIn == null)
-            {
-                dateEnd = DateTime.Now;
+                return debt;
             }
             else
             {
-                dateEnd = (DateTime)dateTurnIn;
-            }
-            if (dateReturn >= dateEnd)
-            {
                 return 0;
             }
-            return bookPrice / 100 * ((int)(dateEnd - dateReturn).TotalDays - 30);
+          
         }
 
     }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Penalty_Correct_3350()
+        {
+            DateTime dateStart = new DateTime(2021, 03, 29);
+            double price = 1000;
+            double ex = 3350;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_155()
+        {
+            DateTime dateStart = new DateTime(2022, 01, 27);
+            double price = 500;
+            double ex = 155;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_1215()
+        {
+            DateTime dateStart = new DateTime(2021, 06, 29);
+            double price = 500;
+            double ex = 1215;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_139()
+        {
+            DateTime dateStart = new DateTime(2022, 01, 27);
+            double price = 449;
+            double ex = 139.19;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+
+        [TestMethod]
+        public void Penalty_Correct_402()
+        {
+            DateTime dateStart = new DateTime(2022, 02, 15);
+            double price = 1299;
+            double ex = 155.88;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+
+        [TestMethod]
+        public void Penalty_Correct_50()
+        {
+            DateTime dateStart = new DateTime(2021, 09, 13);
+            double price = 200;
+            double ex = 334;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_484()
+        {
+            DateTime dateStart = new DateTime(2021, 06, 30);
+            double price = 993;
+            double ex = 2403.06;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_655()
+        {
+            DateTime dateStart = new DateTime(2022, 01, 09);
+            double price = 1337;
+            double ex = 655.13;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_0()
+        {
+            DateTime dateStart = new DateTime(2022, 03, 29);
+            double price = 1337;
+            double ex = 0;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+        [TestMethod]
+        public void Penalty_Correct_4()
+        {
+            DateTime dateStart = new DateTime(2022, 02, 25);
+            double price = 100;
+            double ex = 2;
+            double res = LibraryShiryaev2isp11_17.ClassHelper.Penalty.DebtClient(dateStart, price);
+            Assert.AreEqual(ex, res);
+
+        }
+    }
+}
